48560bd73a977246ecf81290fb6792295e4533cb
Mohit Saini <v-mohit.saini@yatra.com>
2020-04-30 21:04:31 +0530

    added requirements.txt

--- detector.py
+++ detector.py
@@ -1,3 +1,15 @@
+import base64
+import io
+from shapely.geometry import Polygon as shapely_poly
+from shapely.geometry import box
+import argparse
+import pickle
+from pathlib import Path
+from mrcnn.model import MaskRCNN
+import mrcnn.utils
+import mrcnn.config
+import cv2
+import numpy as np
 import git
 import os
 
@@ -5,26 +17,13 @@ if not os.path.exists("Mask_RCNN"):
     print("Cloning M-RCNN repository...")
     git.Git("./").clone("https://github.com/matterport/Mask_RCNN.git")
 
-import numpy as np
-import cv2
-import Mask_RCNN.mrcnn.config
-import Mask_RCNN.mrcnn.utils
-from Mask_RCNN.mrcnn.model import MaskRCNN
-from pathlib import Path
-import pickle
-import argparse
 
-from shapely.geometry import box
-from shapely.geometry import Polygon as shapely_poly
-import io
-import base64
-
-
-class Config(Mask_RCNN.mrcnn.config.Config):
+class Config(mrcnn.config.Config):
     NAME = "model_config"
     IMAGES_PER_GPU = 1
     GPU_COUNT = 1
     NUM_CLASSES = 81
+
 
 config = Config()
 config.display()
@@ -35,10 +34,11 @@ COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 
 print(COCO_MODEL_PATH)
 if not os.path.exists(COCO_MODEL_PATH):
-    Mask_RCNN.mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
+    mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
 
 model = MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=Config())
 model.load_weights(COCO_MODEL_PATH, by_name=True)
+
 
 def get_cars(boxes, class_ids):
     cars = []
@@ -47,6 +47,7 @@ def get_cars(boxes, class_ids):
             cars.append(box)
     return np.array(cars)
 
+
 def compute_overlaps(parked_car_boxes, car_boxes):
     new_car_boxes = []
     for box in car_boxes:
@@ -54,13 +55,13 @@ def compute_overlaps(parked_car_boxes, car_boxes):
         x1 = box[1]
         y2 = box[2]
         x2 = box[3]
-        
+
         p1 = (x1, y1)
         p2 = (x2, y1)
         p3 = (x2, y2)
         p4 = (x1, y2)
         new_car_boxes.append([p1, p2, p3, p4])
-    
+
     overlaps = np.zeros((len(parked_car_boxes), len(new_car_boxes)))
     for i in range(len(parked_car_boxes)):
         for j in range(car_boxes.shape[0]):
@@ -69,7 +70,8 @@ def compute_overlaps(parked_car_boxes, car_boxes):
             polygon1_shape = shapely_poly(pol1_xy)
             polygon2_shape = shapely_poly(pol2_xy)
 
-            polygon_intersection = polygon1_shape.intersection(polygon2_shape).area
+            polygon_intersection = polygon1_shape.intersection(
+                polygon2_shape).area
             polygon_union = polygon1_shape.union(polygon2_shape).area
             IOU = polygon_intersection / polygon_union
             overlaps[i][j] = IOU
@@ -79,7 +81,8 @@ def compute_overlaps(parked_car_boxes, car_boxes):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('video_path', help="Video file")
-    parser.add_argument('regions_path', help="Regions file", default="regions.p")
+    parser.add_argument('regions_path', help="Regions file",
+                        default="regions.p")
     args = parser.parse_args()
 
     regions = args.regions_path
@@ -89,11 +92,11 @@ if __name__ == "__main__":
     VIDEO_SOURCE = args.video_path
     alpha = 0.6
     video_capture = cv2.VideoCapture(VIDEO_SOURCE)
-    video_FourCC    = cv2.VideoWriter_fourcc('M','J','P','G')
-    video_fps       = video_capture.get(cv2.CAP_PROP_FPS)
-    video_size      = (int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                        int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    out = cv2.VideoWriter("out.avi", video_FourCC, video_fps, video_size)
+    video_FourCC = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    video_fps = video_capture.get(cv2.CAP_PROP_FPS)
+    video_size = (int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                  int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    out = cv2.VideoWriter("out_test.avi", video_FourCC, video_fps, video_size)
 
     while video_capture.isOpened():
         success, frame = video_capture.read()
@@ -111,7 +114,7 @@ if __name__ == "__main__":
             max_IoU_overlap = np.max(overlap_areas)
             if max_IoU_overlap < 0.15:
                 cv2.fillPoly(overlay, [np.array(parking_area)], (71, 27, 92))
-                free_space = True      
+                free_space = True
         cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
 
         cv2.imshow('output', frame)
